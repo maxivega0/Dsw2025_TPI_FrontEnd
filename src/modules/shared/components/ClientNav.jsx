4909364ce303a902a2d86b0230484fc6800cdd91
maxivega0 <maximilianovega600@gmail.com>
2025-12-01 20:29:04 -0300

    refinamiento css carrito

--- src/modules/shared/components/ClientNav.jsx
+++ src/modules/shared/components/ClientNav.jsx
@@ -31,9 +31,9 @@ function ClientNav({ setOpenMenu, setMobileView }) {
             setOpenMenu(false), setMobileView(false);
           }}
         >
-          Carrito de Compras
+          <p className="px-1">Carrito de Compras</p>
           {getCartItemCount() > 0 && (
-            <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+            <span className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
               {getCartItemCount()}
             </span>
           )}
